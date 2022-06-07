10364f6fff7c9b35d9b2de7c97632680723d79e1
Boubik <cboubik@gmail.com>
2022-06-08 00:10:10 +0200

    integral + small fix

--- 1.py
+++ 1.py
@@ -14,7 +14,7 @@ def skalarni_soucin(vektor1, vektor2):
 
 def numpy_skalarni_soucin(vektor1, vektor2):
     old_time = time.time()
-    ska_soucin = np.dot(vec1, vec2)
+    ska_soucin = np.dot(vektor1, vektor2)
     print("skalarní součin s numpy trval: " +
           str(time.time() - old_time) + "s")
     return ska_soucin
@@ -40,7 +40,7 @@ def determinant(matice):
         det += mezi_plus
         det -= mezi_minus
         #print(" ")
-    print("Determinant s numpy trval: " + str(time.time() - old_time) + "s")
+    print("Determinant trval: " + str(time.time() - old_time) + "s")
     return det
 
 
@@ -51,6 +51,27 @@ def numpy_determinant(matice):
     return det
 
 
+def integral(bod1,bod2,step):
+    old_time = time.time()
+    rngup = int(1/step)
+    x1 = [x * step for x in range(rngup*bod1,rngup*bod2+1)]    
+    resault = 0
+    for i in x1:    
+        resault = resault + (3*i+2)*step
+    end_time = time.time()
+    print("integrál trval: " + str(time.time() - old_time) + "s")
+    return resault
+
+def numpy_integral(start, end, step):
+    old_time = time.time()
+    step = int(1/step)
+    x = np.linspace(start, end, step)
+    f = 3*x+2
+    resault = trapz(f, x)
+    print("integrál s numpy trval: " + str(time.time() - old_time) + "s")
+    return resault
+
+
 if __name__ == "__main__":
     # skalánrí součin
     vec1 = [2, 4, 6]
@@ -58,7 +79,14 @@ if __name__ == "__main__":
     num_vec1 = np.array([2, 4, 6])
     num_vec2 = np.array([3, 5, 7])
     print(skalarni_soucin(vec1, vec2))
-    print(skalarni_soucin(num_vec1, num_vec2))
+    print(numpy_skalarni_soucin(num_vec1, num_vec2))
+
+    # integral
+    point1 = 0
+    point2 = 10
+    step = 0.000001
+    print(integral(point1, point2, step))
+    print(numpy_integral(point1, point2, step))
 
     # determinant
     vec = [[6, 1, 1], [4, -2, 5], [2, 8, 7]]
